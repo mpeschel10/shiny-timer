5bedee189126b7dd455af6a60443a2b2a594d10c
Mark Peschel <mpeschel10@gmail.com>
2022-11-13 11:02:49 -0500

    push_to_server now points to mpeschel10.com; uses rsync.

--- push_to_server.py
+++ push_to_server.py
@@ -1,71 +1,50 @@
 #!/bin/python3
 
+import subprocess
 import shutil
 
 from pathlib import Path
 script_dir = Path(__file__).resolve().parent
-server_dir = Path('/var/www/timer')
+key_path = Path('/home/mpeschel/.ssh/dobby')
+target_string = 'root@mpeschel10.com:/var/www/timer'
 
-strings_to_copy = [
-    'sounds',
+whitelist = [
     'sounds/*.ogg',
     'timer.html',
     'timer.js',
     'licenses.txt'
 ]
 
-local_paths = set()
-for s in strings_to_copy:
+# rsync does have a built-in whitelist/blacklist functionality
+#  in the form of include/exclude rules.
+# But I spent half an hour poking at that and couldn't work it out.
+# So just implement the file-list builder in python instead.
+source_paths = []
+for s in whitelist:
     if '*' in s:
         for child in script_dir.glob(s):
-            local_path = child.relative_to(script_dir)
-            local_paths.add(local_path)
+            source_paths.append(child.relative_to(script_dir))
     else:
-        local_paths.add(Path(s))
+        source_paths.append(Path(s))
 
-for abs_path in server_dir.rglob('**/*'):
-    server_path = abs_path.relative_to(server_dir)
-    if not server_path in local_paths:
-        input('Going to delete {} from server; press enter'.format(server_path))
-        if abs_path.is_dir():
-            abs_path.rmdir()
-        else:
-            abs_path.unlink()
+# rsync flags are basically -a|--archive but not recursive.
+rsync_flags = '-' + ''.join([
+    #'r', # recursive (use whitelist instead)
+    'R', # preserve relative path, necessary due to file-list-as-whitelist
+    'l', # copy symlinks
+    'p', # copy permissions
+    't', # copy times
+    'o', # copy owner
+    'g', # copy group
+    'v', # verbose
+    #' --devices', # preserve device files. (I don't need this)
+    #' --specials', # preserve special files. (I don't need this)
+])
 
-for local_path in local_paths:
-    abs_path = server_dir.joinpath(local_path)
-    if abs_path.exists():
-        if abs_path.is_dir():
-            if local_path.is_dir():
-                continue
-            else:
-                raise Exception('{} is a directory on server but a file locally. Please fix manually.'.format(local_path))
-        if local_path.is_dir():
-            raise Exception('{} is a file on server but a directory locally. Please fix manually.'.format(local_path))
+ssh_cmd = ['ssh', '-i', key_path]
+ssh_cmd_str = ' '.join(str(s) for s in ssh_cmd)
+rsync_cmd = ['rsync', rsync_flags, '-e', ssh_cmd_str] + source_paths + [target_string]
 
-        if local_path.stat().st_mtime == abs_path.stat().st_mtime:
-            continue
-    
-    if local_path.is_dir():
-        abs_path.mkdir()
-    else:
-        print('Copying {} to {}'.format(local_path, abs_path))
-        shutil.copy2(local_path, abs_path)
+#print(' '.join(str(s) for s in rsync_cmd))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+subprocess.run(rsync_cmd)
