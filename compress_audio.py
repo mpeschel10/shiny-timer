83cc1e6f3ce3bc333d83454efd9e28647c446270
Mark Peschel <mpeschel10@gmail.com>
2022-12-20 15:24:24 -0500

    Normalize loudness.

--- compress_audio.py
+++ compress_audio.py
@@ -2,18 +2,18 @@ import sys, subprocess
 
 from pathlib import Path
 script_dir = Path(__file__).resolve().parent
+sound_dir = script_dir.joinpath('sounds')
 
 flag_force = False
 
 def main():
     #global flag_force
-    for old_loop in script_dir.glob("*-loop.wav"):
+    # For speed, this script only reencodes files if -f is present
+    for old_loop in sound_dir.glob("*-loop.wav"):
         new_loop = old_loop.parent.joinpath(old_loop.stem + '.ogg')
-        print(old_loop)
-        print(new_loop)
         if flag_force or not new_loop.exists():
-            # -i <input file> -c<onvert>:a<udio> 'libvorbis' <output file>
-            cmd = ['ffmpeg', '-i', old_loop, '-c:a', 'libvorbis', new_loop]
+            # -i <input file> -filter:a<udio> 'loudnorm' -c<onvert>:a<udio> 'libvorbis' <output file>
+            cmd = ['ffmpeg', '-i', old_loop, '-filter:a', 'loudnorm', '-c:a', 'libvorbis', new_loop]
             #print('I will run {}'.format(' '.join(str(c) for c in cmd)))
             subprocess.run(cmd)
 
